8bb52778b797d2b93d7820256fa04b2e7e46631f
edupoly <info.edupoly@gmail.com>
2022-07-27 13:15:43 +0530

    usereducer counter

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import Counter from './Counter';
 function App() {
   
   return (
     <div className="App">
       <h1>Hello ReactJS Application development</h1>
-      
+      <Counter></Counter>
     </div>
   );
 }
--- myapp/src/Counter.jsx
+++ myapp/src/Counter.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
-
+const initialState = {
+    count:1
+}
+function counterreducer(state,action){
+    if(action.type==='INC'){
+        return {...state,count:state.count+1}
+    }
+    if(action.type==='DEC'){
+        return {...state,count:state.count-1}
+    }
+    return state
+}
 function Counter() {
-  return (
-    <div className='betterview'>
-        <h1>Counter</h1>
-    </div>
-  )
+    const [state, dispatch] = React.useReducer(counterreducer,initialState)
+    return (
+        <div className='betterview'>
+            <h1>Counter:{state.count}</h1>
+            <button onClick={()=>{dispatch({type:'INC'})}}>Increment</button>
+            <button onClick={()=>{dispatch({type:'DEC'})}}>Decrement</button>
+        </div>
+    )
 }
 
 export default Counter
