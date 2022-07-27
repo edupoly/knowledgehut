6b93161d6edbeb331e2fbdc5ab120cbc35a1fc68
edupoly <info.edupoly@gmail.com>
2022-07-27 13:06:48 +0530

    plain

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,8 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import Counter from './Counter';
-import Foodcart from './Foodcart';
 function App() {
   
   return (
